d69ddca26b8d4ed93a5f6b8ca385c6411cd639b7
Outermeasure <adrian_scp@outlook.com>
2013-12-07 03:14:14 +0200

    Removed the extra include overhead from common.h to contain only forward declarations.

--- userprog/common.h
+++ userprog/common.h
@@ -1,8 +1,6 @@
 #ifndef USERPROG_COMMON_H
 #define USERPROG_COMMON_H
-#include <list.h>
-#include <hash.h>
-#include <threads/synch.h>
+/* forward declarations */
 
 /* Process identifier type.
    You can redefine this to whatever type you like. */
@@ -10,31 +8,14 @@ typedef int pid_t;
 
 static pid_t const PID_ERROR = -1;
 
-/* forward declarations */
-struct thread;
+struct process_t;
+typedef struct process_t process_t;
+
 
 enum pstatus_t {
 	ALIVE, 
 	KILLED, 
 	DEAD
 };
-
-struct process_t {
-	/* process id of this process */
-	pid_t pid;
-	/* pid (process id) of this process's parent */
-	pid_t ppid;
-	/* status of the process: one of ALIVE, KILLED, DEAD. */
-	enum pstatus_t status;
-	/* the code returned by the process at exit */
-	int exit_code;
-	/* List of file descriptors. */
-	struct list owned_file_descriptors;
-	/* Pointer to element in hash table */
-	struct hash_elem h_elem;
-	/* wating semaphore instead of thread_block/unblock hackarounds */
-	struct semaphore process_semaphore;
-};
-typedef struct process_t process_t;
 
 #endif
--- userprog/process.h
+++ userprog/process.h
@@ -1,8 +1,29 @@
 #ifndef USERPROG_PROCESS_H
 #define USERPROG_PROCESS_H
 
-#include "userprog/common.h"
+#include <list.h>
+#include <hash.h>
+#include "threads/synch.h"
 #include "threads/thread.h"
+#include "userprog/common.h"
+
+
+struct process_t {
+	/* process id of this process */
+	pid_t pid;
+	/* pid (process id) of this process's parent */
+	pid_t ppid;
+	/* status of the process: one of ALIVE, KILLED, DEAD. */
+	enum pstatus_t status;
+	/* the code returned by the process at exit */
+	int exit_code;
+	/* List of file descriptors. */
+	struct list owned_file_descriptors;
+	/* Pointer to element in hash table */
+	struct hash_elem h_elem;
+	/* wating semaphore instead of thread_block/unblock hackarounds */
+	struct semaphore process_semaphore;
+};
 
 pid_t process_execute (const char *file_name);
 int process_wait (pid_t);
