0abdfbbcc99fa346badfc28b927096d710b69884
Andrei <andrei@andrei-laptop.(none)>
2014-01-08 01:35:31 +0200

    Frame table initial implementation

--- vm/frame.c
+++ vm/frame.c
@@ -1,69 +1,186 @@
-#include "vaddr.h"
-#include "frame.h"
-#include "synch.h"
+#include "threads/vaddr.h"
+#include "threads/synch.h"
+#include "threads/palloc.h"
+#include "threads/malloc.h"
+#include "threads/thread.h"
+#include "vm/frame.h"
+#include "userprog/pagedir.h"
 
+#include <string.h>
+
+/* The frametable */
 static struct hash frame_table;
+
+/* A lock for frame_table synch access*/
 struct lock ft_lock;
 
-size_t get_frame_table_size()
+frame* frame_lookup (const void *kpage);
+static bool frame_less (const struct hash_elem *a_, const struct hash_elem *b_, void *aux UNUSED);
+static unsigned frame_hash (const struct hash_elem *f_, void *aux UNUSED);
+
+/*hash function for the frame table.
+It should be computed using the kpage field
+of the frame */
+static unsigned frame_hash (const struct hash_elem *f_, void *aux UNUSED)
 {
-	return PGSIZE * frame_table.elem_cnt;
+	const frame *f = hash_entry (f_, frame, he);
+	return hash_int ((int)f->kpage);
+}
+
+static bool frame_less (const struct hash_elem *a_, const struct hash_elem *b_, void *aux UNUSED)
+{
+	const frame *a = hash_entry (a_, frame, he);
+	const frame *b = hash_entry (b_, frame, he);
+
+	return a->kpage < b->kpage;
+}
+
+frame* frame_lookup (const void *kpage)
+{
+	struct frame f;
+	struct hash_elem *e;
+
+	f.kpage = kpage;
+	e = hash_find((&frame_table), &f.he);
+
+	return e != NULL ? hash_entry (e, frame, he) : NULL ;
 }
 
 /*
  * Initializes the frame table
  */
-void ft_init()
+void ft_init(void)
 {
-	hash_init(&frame_table);
+	hash_init(&frame_table, frame_hash, frame_less, NULL);
 }
 
 /**
- * Adds a page to the frame table
+ * Insert a frame into the frame table
  */
-void ft_add_page(void *phys_add, void *page)
+void ft_insert_frame(frame *f)
 {
+	f->pinned = true;
 	lock_acquire(&ft_lock);
-	// insert page in frame_table hash
+	hash_insert(&frame_table, &(f->he));
+	f->pinned = false;
 	lock_release(&ft_lock);
 }
 
 /**
- * Deletes a page from the frame_table
+ * Removes the frame given from the
+ * frame_table and frees the space
+ * occupied by the frame table entry.
  */
-void ft_evict_page(void *phys_add)
+void ft_remove_frame(frame* frame)
 {
+	frame->pinned = true;
 	lock_acquire(&ft_lock);
-	// delete page from frame_table hash
+	hash_delete(&frame_table, &(frame->he));
 	lock_release(&ft_lock);
+	free(frame);
 }
 
-/**
- * Search in the frame table by page
- */
-void* ft_search_page(void *page)
+/*allocates a page within a frame
+  and returns the allocated frame
+  if there is an unused frame.
+  Otherwise, it tries to evict a
+  frame and return it. If no frame
+  is unused and no frame can be evicted
+  it returns NULL. */
+frame* 	ft_alloc_frame(bool zero_page, const void *page_u_addr)
 {
-	lock_acquire(&ft_lock);
-	// search page in frame_table hash
-	lock_release(&ft_lock);
+	enum palloc_flags flags = PAL_USER | (zero_page ? PAL_ZERO : 0);
+
+	void *page_k_addr = palloc_get_page(flags);
+	if (page_k_addr != NULL ) {
+		frame *f = (frame *) malloc(sizeof(frame));
+		f->kpage = page_k_addr;
+		f->upage = page_u_addr;
+		f->pinned = false;
+		ft_insert_frame(f);
+		return f;
+	} else {
+		frame *lru_f = ft_get_lru_frame();
+		if (lru_f != NULL )
+		{
+			lru_f->pinned = true;
+
+			if(!ft_evict_frame(lru_f, false))
+			{
+				return NULL;
+			}
+
+			lru_f->upage = page_u_addr;
+			if (zero_page)
+				memset(lru_f->kpage, 0, 1);
+
+			lru_f->pinned = false;
+		}
+
+		return lru_f;
+	}
+
+	return NULL ; //not reachable
+}
+
+/*
+  Finds the least recently used frame
+*/
+frame* ft_get_lru_frame(void)
+{
+	//TODO: implement clock-algorithm or second-chance
 	return NULL;
 }
 
-/**
- * Returns the page corresponding
- * to phys_add
- */
-
-void* ft_get_page(void *phys_add)
+/* Tries to evict the frame given. If the
+ * page within the frame was modified, the page
+ * is written to swap. If dealloc is requested,
+ * the memory is freed, otherwise, the memory is
+ * kept for another page to be used. Returns true
+ * if eviction was successful, false otherwise.
+*/
+bool ft_evict_frame(frame* frame, bool dealloc_page)
 {
-	void *page = NULL;
-	lock_acquire(&ft_lock);
-	//page = hash_table[phys_add];
-	lock_release(&ft_lock);
-	return page;
+	struct thread *t = thread_current();
+
+	if( pagedir_is_dirty(t->pagedir, frame->upage) )
+	{
+		frame->pinned = true;
+		//TODO:swap out the page
+		//supl_pte = get_supl_pte(current_process, frame.upage);
+		//supl_pte.slot_no = swap_out(frame.kpage)
+		frame->pinned = false;
+		//if(supl_pte.slot_no < 0)
+			//	return false
+	}
+	if( dealloc_page )
+	{
+		pagedir_clear_page(t->pagedir, frame->upage);
+		palloc_free_page(frame->kpage);
+		ft_remove_frame(frame);
+	}
+
+	return true;
 }
 
-frame ft_get_unused_frame()
+/*
+ * Pins the frame, namely disqualifies it from
+ * being evicted by the LRU algorithm
+ */
+void ft_pin_frame(const void *uaddr)
 {
-	//eviction algorithm
+	void *kpage = pagedir_get_page(thread_current()->pagedir, uaddr);
+	frame *f = frame_lookup(kpage);
+	f->pinned = false;
+}
+
+/*
+ * Unpins the frame, making it available to
+ * be evicted by the LRU algorithm
+ */
+void ft_unpin_frame(const void *uaddr)
+{
+	void *kpage = pagedir_get_page(thread_current()->pagedir, uaddr);
+	frame *f = frame_lookup(kpage);
+	f->pinned = false;
 }
--- vm/frame.h
+++ vm/frame.h
@@ -1,25 +1,40 @@
 #ifndef FRAME_H
 #define FRAME_H
 
-#include "hash.h"
+#include <hash.h>
 
 struct frame
 {
-	void *phys_add; //physical address - key in hash_t
-	void *page_add;
+	//kernel virtual address of page
+	void *kpage;
+
+	//user virtual address of page
+	void *upage;
+
+	//if frame is pinned, the contained page can not be evicted
 	bool  pinned;
-	struct hash_elem he;
+
+	struct hash_elem he; //needed for hash_table
 };
 
 typedef struct frame frame;
 
-size_t ft_get_size();
-void   ft_init();
-void   ft_add_page(void *phys_add, void *page);
-void   ft_evict_page(void *phys_add);
-void*  ft_search_page(void *page);
-void*  ft_get_page(void *phys_add);
-frame  ft_get_unused_frame();
+//initializes the frame-table
+void	ft_init(void);
+void 	ft_insert_frame(frame *f);
+void 	ft_remove_frame(frame *f);
+frame* 	ft_alloc_frame(bool zero_page, const void *upage);
+frame* 	ft_get_lru_frame(void);
+bool 	ft_evict_frame(frame* frame, bool dealloc_page);
+void 	ft_pin_frame(const void *uaddr);
+void 	ft_unpin_frame(const void *uaddr);
+
+//size_t ft_get_size(void);
+//void   ft_add_page(void *phys_add, void *page);
+//void   ft_evict_page(void *phys_add);
+//void*  ft_search_page(void *page);
+//void*  ft_get_page(void *phys_add);
+//frame*  ft_get_unused_frame(void);
 
 
 #endif
