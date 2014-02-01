f18247e82e5537949b86fe54476532f648bcd9dc
Andrei Zene <andrei.zene@yahoo.com>
2014-02-01 20:03:45 +0200

    Added verbose option to runtest.sh. Verbose option prints progress of test when set.

--- filesys/runtest.sh
+++ filesys/runtest.sh
@@ -1,19 +1,58 @@
 
 #!/bin/sh -x
 
-if [ "$#" -ne 1 ]; then
-    echo "Wrong number of parameters. You should call it: $0 test_name"
-    exit 
-fi
+usage()
+{
+    echo "Usage: $0 [OPTIONS] test_name"
+    echo "Options: "
+    echo "     -v | --verbose (verbose mode)"
+    echo "     -h | --help (this message)"
+}
+
+VERBOSE_OPT=""
+
+while :
+do
+    case $1 in
+	-h | --help | -\?)
+	    usage
+	    exit 0
+	    ;;
+	-v | --verbose)
+	    VERBOSE_OPT="VERBOSE=1"
+	    shift
+	    ;;
+	--)
+	    #no more options.
+	    shift
+	    break;
+	    ;;
+	-*)
+	    echo "No such option: $1" >&2
+	    usage
+	    exit 0
+	    ;;
+	*)
+	    # no more options.
+	    break;
+    esac
+done
  
+if [ $# -ne 1 ]; then
+    usage
+    exit 0;
+fi;
+
+test_name=$1
+
 make
-BINARY=`find ./build/tests/ -name $1.o`
-if [ -z "$BINARY" ]; then
+BINARY=`find ./build/tests/ -name $test_name.o`
+if [ ! "$BINARY" ]; then
     echo
-    echo "Test $1 was not found. Check for spelling mistakes"
+    echo "Test \"$test_name\" was not found. Check for spelling mistakes"
     echo
     exit
 fi
  
-rm "`dirname $BINARY`/$1"
-make "`dirname $BINARY`/$1.result"
+rm "`dirname $BINARY`/$test_name"
+make "`dirname $BINARY`/$test_name.result" $VERBOSE_OPT 
